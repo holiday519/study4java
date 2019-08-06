d5008e775d1fc87e84540ae07969748a8dffb73d
zhengyi <zhengyi19870519@gmail.com>
2019-08-06 22:09:01 +0800

    Update BST.java
    BST

--- src/main/java/com/ee/exercises/tree/BST.java
+++ src/main/java/com/ee/exercises/tree/BST.java
@@ -7,11 +7,13 @@ public class BST<Key extends  Comparable<Key>, Value> {
         private Value val;
         private Node left, right;
         private int N;
+        private int H;
 
-        public Node(Key key, Value val, int N) {
+        public Node(Key key, Value val, int N, int H) {
             this.key = key;
             this.val = val;
             this.N = N;
+            this.H = H;
         }
     }
 
@@ -24,6 +26,15 @@ public class BST<Key extends  Comparable<Key>, Value> {
     private int size(Node x) {
         if (x == null) return 0;
         else return x.N;
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    public int height(Node x) {
+        if (x == null) return 0;
+        else return x.H;
     }
 
     public Value get(Key key) {
@@ -43,29 +54,30 @@ public class BST<Key extends  Comparable<Key>, Value> {
     }
 
     private Node put(Node x, Key key, Value val) {
-        if (x == null) return new Node(key, val, 1);
+        if (x == null) return new Node(key, val, 1, 1);
         int cmp = key.compareTo(x.key);
         if (cmp < 0) x.left = put(x.left, key, val);
         else if (cmp > 0) x.right = put(x.right, key, val);
         else x.val = val;
         x.N = size(x.left) + size(x.right) + 1;
+        x.H = height(x.left) > height(x.right) ? height(x.left)+1 : height(x.right)+1;
         return x;
     }
 
-    public int height() {
-        return height(root);
-    }
-
-    private int height(Node x) {
-        if (x == null) return 0;
-        int leftHeight = height(x.left);
-        int rightHeight = height(x.right);
-        if (leftHeight >= rightHeight) {
-            return leftHeight + 1;
-        } else {
-            return rightHeight + 1;
-        }
-    }
+//    public int height() {
+//        return height(root);
+//    }
+//
+//    private int height(Node x) {
+//        if (x == null) return 0;
+//        int leftHeight = height(x.left);
+//        int rightHeight = height(x.right);
+//        if (leftHeight >= rightHeight) {
+//            return leftHeight + 1;
+//        } else {
+//            return rightHeight + 1;
+//        }
+//    }
 
     public static void main(String[] args) {
         BST<Integer, String> demo = new BST<>();
@@ -74,8 +86,10 @@ public class BST<Key extends  Comparable<Key>, Value> {
         demo.put(6,"c");
         demo.put(1,"d");
         demo.put(7,"e");
+        demo.put(2,"f");
 
         System.out.println(demo.height());
+        System.out.println(demo.size());
     }
 
 }
