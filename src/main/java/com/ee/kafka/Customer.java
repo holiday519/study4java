409361bfece4bd95a435088ee14c46a252a62392
zhengyi <zheng.yi@kuyun.com>
2020-01-12 23:57:23 +0800

    提交

---
+++ src/main/java/com/ee/kafka/Customer.java
@@ -0,0 +1,16 @@
+package com.ee.kafka;
+
+public class Customer {
+	private int customerID;
+	private String customerName;
+	public Customer(int customerID, String customerName) {
+		this.customerID = customerID;
+		this.customerName = customerName;
+	}
+	public int getID() {
+		return customerID;
+	}
+	public String getName() {
+		return customerName;
+	}
+}
